2c449af380cd746dff668832bae2451ff9b33e06
FanXiaobin <527256662@qq.com>
2019-08-26 14:16:24 +0800

    添加属性
    添加属性

--- FF_LiveSendGift/LiveGiftShowView/LiveGiftShowModel.h
+++ FF_LiveSendGift/LiveGiftShowView/LiveGiftShowModel.h
@@ -19,6 +19,9 @@
 
 @property (nonatomic, assign) NSUInteger currentNumber;/** 当前送礼数量 */
 
+///要显示的数字 默认为0 累加1  如果大于0 就直接显示这个数字不累加
+@property (nonatomic, assign) NSUInteger showNumber;/** 直接显示的数量 */
+
 // 连续动画时使用
 @property (nonatomic, assign) NSUInteger toNumber;/** 连续增加的数量 */
 @property (nonatomic, assign) CGFloat interval;/** 连续增加时动画间隔 */
